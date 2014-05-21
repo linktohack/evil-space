b9f5aafc94d262616af1e57a571a45107e6441b5
Quang-Linh LE <linktohack@gmail.com>
2014-05-21 17:09:04 +0200

    Natural key movement
    i.e. SPC always move next while DEL in reverse direction

--- evil-space.el
+++ evil-space.el
@@ -1,4 +1,17 @@
-(defalias 'evil-space-next 'evil-next-line)
-(defalias 'evil-space-prev 'evil-previous-line)
-(define-key evil-normal-state-map (kbd "SPC") 'evil-space-next)
-(define-key evil-normal-state-map (kbd "DEL") 'evil-space-prev)
+(setq evil-space-next-key (kbd "SPC"))
+(setq evil-space-next-key (kbd "SPC"))
+(defadvice evil-next-line (before evil-space-next-line activate)
+  (define-key evil-normal-state-map (kbd "SPC") 'evil-next-line)
+  (define-key evil-normal-state-map (kbd "DEL") 'evil-previous-line))
+
+(defadvice evil-previous-line (before evil-space-previous-line activate)
+  (define-key evil-normal-state-map (kbd "SPC") 'evil-next-line)
+  (define-key evil-normal-state-map (kbd "DEL") 'evil-previous-line))
+
+;; (defadvice evil-next-line (before evil-space-next-line activate)
+;;   (defalias 'evil-space-next 'evil-next-line)
+;;   (defalias 'evil-space-prev 'evil-previous-line))
+
+;; (defadvice evil-previous-line (before evil-space-previous-line activate)
+;;   (defalias 'evil-space-next 'evil-previous-line)
+;;   (defalias 'evil-space-prev 'evil-next-line))
