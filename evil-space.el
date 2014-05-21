42e39efa3ee3d5190b8edca9b2a83d19ab30dbd4
Quang-Linh LE <linktohack@gmail.com>
2014-05-21 18:46:28 +0200

    Redefine motion instead of relying on defadvice

--- evil-space.el
+++ evil-space.el
@@ -1,17 +1,50 @@
 (setq evil-space-next-key (kbd "SPC"))
-(setq evil-space-next-key (kbd "SPC"))
-(defadvice evil-next-line (before evil-space-next-line activate)
-  (define-key evil-normal-state-map (kbd "SPC") 'evil-next-line)
-  (define-key evil-normal-state-map (kbd "DEL") 'evil-previous-line))
+(setq evil-space-previous-key (kbd "DEL"))
+(setq evil-space-last-count 1)
 
-(defadvice evil-previous-line (before evil-space-previous-line activate)
-  (define-key evil-normal-state-map (kbd "SPC") 'evil-next-line)
-  (define-key evil-normal-state-map (kbd "DEL") 'evil-previous-line))
+(evil-define-motion evil-space-next-line (count)
+  "Setup evil-space and Move the cursor COUNT lines down."
+  :type line
+  (setq evil-space-last-count (or count 1))
+  (define-key evil-motion-state-map evil-space-next-key
+    '(lambda () (interactive) (evil-next-line evil-space-last-count)))
+  (define-key evil-motion-state-map evil-space-previous-key
+    '(lambda () (interactive) (evil-next-line evil-space-last-count)))
+  (evil-next-line evil-space-last-count))
 
-;; (defadvice evil-next-line (before evil-space-next-line activate)
-;;   (defalias 'evil-space-next 'evil-next-line)
-;;   (defalias 'evil-space-prev 'evil-previous-line))
+(evil-define-motion evil-space-previous-line (count)
+  "Setup evil-space and Move the cursor COUNT lines up."
+  :type line
+  (setq evil-space-last-count (or count 1))
+  (define-key evil-motion-state-map evil-space-next-key
+    '(lambda () (interactive) (evil-previous-line evil-space-last-count)))
+  (define-key evil-motion-state-map evil-space-previous-key
+    '(lambda () (interactive) (evil-next-line evil-space-last-count)))
+  (evil-previous-line evil-space-last-count))
 
-;; (defadvice evil-previous-line (before evil-space-previous-line activate)
-;;   (defalias 'evil-space-next 'evil-previous-line)
-;;   (defalias 'evil-space-prev 'evil-next-line))
+(evil-define-motion evil-space-search-next (count)
+  "Setup evil-space and Repeat the last search."
+  :jump t
+  :type exclusive
+  (setq evil-space-last-count (or count 1))
+  (define-key evil-motion-state-map evil-space-next-key
+    '(lambda () (interactive) (evil-search-next evil-space-last-count)))
+  (define-key evil-motion-state-map evil-space-previous-key
+    '(lambda () (interactive) (evil-search-previous evil-space-last-count)))
+  (evil-search-next evil-space-last-count))
+
+(evil-define-motion evil-space-search-previous (count)
+  "Setup evil-space and Repeat the last search in the opposite direction."
+  :jump t
+  :type exclusive
+  (setq evil-space-last-count (or count 1))
+  (define-key evil-motion-state-map evil-space-next-key
+    '(lambda () (interactive) (evil-search-previous evil-space-last-count)))
+  (define-key evil-motion-state-map evil-space-previous-key
+    '(lambda () (interactive) (evil-search-next evil-space-last-count)))
+  (evil-search-previous evil-space-last-count))
+
+(define-key evil-motion-state-map "j" 'evil-space-next-line)
+(define-key evil-motion-state-map "k" 'evil-space-previous-line)
+(define-key evil-motion-state-map "n" 'evil-space-search-next)
+(define-key evil-motion-state-map "N" 'evil-space-search-previous)
